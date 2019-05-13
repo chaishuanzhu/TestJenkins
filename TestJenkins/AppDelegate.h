6ec6e3038f5f526c83f5412fc78bb2bfbf217054
飞鱼 <870027381@qq.com>
2019-05-13 18:03:43 +0800

    Initial Commit

---
+++ TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by 飞鱼 on 2019/5/13.
+//  Copyright © 2019 xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestJenkins/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestJenkins
+//
+//  Created by 飞鱼 on 2019/5/13.
+//  Copyright © 2019 xxx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
